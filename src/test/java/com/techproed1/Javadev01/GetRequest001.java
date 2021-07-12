0bf1885ef624e4e5f3ba29af5fe45e2776a327b3
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-12 18:43:02 -0500

    m

---
+++ src/test/java/com/techproed1/Javadev01/GetRequest001.java
@@ -0,0 +1,28 @@
+package com.techproed1.Javadev01;
+
+import org.junit.Test;
+
+public class GetRequest001 {
+
+	
+	/*
+	 Gherkin Language:
+	 Given: Declare prerequisets
+	 When:It defines the actions user will perform
+	 Then: Talk about outputs
+	 And: It can be used in Given, When, Then, if you have multiple options. for more readable;
+	 */
+	/*
+	 When: I send a GET request to the URL https://restful-booker.herokuapp.com/booking/3
+	 Then: HTTP Status code should be 200
+	 And:Content type should be JSON
+	 And: Status Line should be HTTP/1.1 200 OK
+	 */
+	
+	
+	//To create a test method we use test annotation which comes from junit.
+	@Test
+	public void get01() {
+		
+	}
+}
