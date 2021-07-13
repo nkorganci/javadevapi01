ee23a69473f87501def58cf8e945f20ed73ff4bf
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-13 11:45:13 -0500

    m

--- src/test/java/com/techproed1/Javadev01/GetRequest001.java
+++ src/test/java/com/techproed1/Javadev01/GetRequest001.java
@@ -2,6 +2,11 @@ package com.techproed1.Javadev01;
 
 import org.junit.Test;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+
+
 public class GetRequest001 {
 
 	
@@ -23,6 +28,24 @@ public class GetRequest001 {
 	//To create a test method we use test annotation which comes from junit.
 	@Test
 	public void get01() {
+	//If you need to test an API we need to do 4steps:
+		
+		//1.Step: send URL
+		String url = "https://restful-booker.herokuapp.com/booking/3";
+		
+		//2.Step: Set the expected data
+		//3.Step: Send the request to the API
+		Response response = given().accept("application/json").when().get(url);
+		response.prettyPrint();
+		//4.step: Make assertions
+		response.then().assertThat().statusCode(200).contentType(ContentType.JSON).statusLine("HTTP/1.1 200 OK");// "application/json" is also working.
+		
+		//How to print content-type, status code, status line
+		System.out.println("contentType: " + response.getContentType());
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeaders());
+		System.out.println(response.getHeader("Date"));
 		
 	}
 }
